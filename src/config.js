9a7a681b7a0242f986be8f888a750a9f0aa92090
YCH <943696882@qq.com>
2025-03-20 14:21:50 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const Config = (config) => {
+    return config;
+}
+
+export default Config({
+  name: "My Awesome Websites",
+  url: "https://my-awesome-websites.com",
+  title: "My Awesome Websites - Creative, Responsive, Professional!",
+  description: "Discover My Awesome Websites! A collection of creative, responsive, and professional web designs. Explore modern UI/UX, innovative features, and seamless user experiences. Visit my-awesome-websites.com to see our portfolio!",
+  keywords: "My Awesome Websites, web design, responsive websites, professional websites, web development, UI/UX, portfolio, modern websites, creative design, web solutions, frontend development, backend development",
+})
